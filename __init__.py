b63197b62601e829832604f452ffb90e47e674b4
Yaloke <116218624+Yaloke@users.noreply.github.com>
2024-02-12 09:50:53 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -30,6 +30,10 @@ def mongraphique():
 @app.route('/')
 def hello_world():
     return render_template('hello.html')
+
+@app.route("/histogramme/")
+def monhistogramme():
+    return render_template("histogramme.html")
   
 if __name__ == "__main__":
   app.run(debug=True)

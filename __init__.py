0cd42b9805b43d17d455493448ffa5316fdcca94
Yaloke <116218624+Yaloke@users.noreply.github.com>
2024-02-12 10:17:57 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -9,7 +9,7 @@ app = Flask(__name__) #Hello World !
 
 @app.route("/contact/")
 def MaPremiereAPI():
-    return "<h2>Ma page de contact</h2>"
+    return render_template('contact.html')
 
 @app.route('/paris/')
 def meteo():
